b3d2603c16f11f3f230796562f68c34429f2b259
Thomas Scholtes <thomas@monadic.xyz>
2019-08-08 17:31:32 +0200

    Add ledger tests
    We add tests for the ledger code that can run without the Parity Wasm
    Smart Contract environment.
    
    To accomplish this we abstract over the primitives provided by
    `pwasm_ethereum` and introcude the `pwasm::Env` trait.

---
+++ ledger/src/pwasm.rs
@@ -0,0 +1,64 @@
+//! Access to Parity Wasm primitives
+//!
+//! [Env] defines a trait for all methods the ledger runtime needs to provide. When the ledger runs
+//! as a Parity Wasm Smart Contract thhe functionality is provided by `Pwasm` using the
+//! `pwasm_ethereum` crate.
+//!
+//! For testing purposes a [TestEnv] implementation is provided.
+//!
+//! Oscoin Ledger code must not use `pwasm_ethereum` directly. It must only use functionality
+//! exposed by [Env].
+
+#[doc(inline)]
+pub use pwasm_abi::types::*;
+
+/// Primitives allowing access to the smart contract environment.
+pub trait Env {
+    fn write(&mut self, key: &H256, value: &[u8; 32]);
+    fn read(&self, key: &H256) -> [u8; 32];
+}
+
+/// Implements [Env] for the Parity Wasm Smart Contract environment using the `pwasm_ethereum` crate.
+pub struct Pwasm;
+
+impl Env for Pwasm {
+    fn write(&mut self, key: &H256, value: &[u8; 32]) {
+        pwasm_ethereum::write(key, value)
+    }
+
+    fn read(&self, key: &H256) -> [u8; 32] {
+        pwasm_ethereum::read(key)
+    }
+}
+
+#[cfg(test)]
+use std::collections::HashMap;
+
+/// Implements [Env] using a [HashMap].
+#[cfg(test)]
+pub struct TestEnv {
+    state: HashMap<H256, [u8; 32]>,
+}
+
+#[cfg(test)]
+impl TestEnv {
+    pub fn new() -> TestEnv {
+        TestEnv {
+            state: HashMap::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl Env for TestEnv {
+    fn write(&mut self, key: &H256, value: &[u8; 32]) {
+        self.state.insert(*key, *value);
+    }
+
+    fn read(&self, key: &H256) -> [u8; 32] {
+        match self.state.get(key) {
+            Some(value) => *value,
+            None => Default::default(),
+        }
+    }
+}
