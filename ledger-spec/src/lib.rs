a5329ef82e8e236dfe8b0e484c34eba9e32b3d3f
Alexandre Baldé <alexandrer_b@outlook.com>
2019-08-12 19:10:58 +0100

    Finalize specification of checkpoint transaction
    As an aside, the ledger spec now compiles, and it is a part of the
    build process.

--- ledger-spec/src/lib.rs
+++ ledger-spec/src/lib.rs
@@ -1,3 +1,10 @@
+// These two module-wide attributes are used to disable "unused variable" and
+// "field is never used" warnings when compiling this file.
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::marker::PhantomData;
+
 /// This is a specification document meant to approximate the Ledger described in
 /// Oscoin whitepaper into concrete Rust code.
 /// However, it is not meant to be an exact implementation.
@@ -42,15 +49,18 @@ pub enum RegisterProjectError {
 
     /// The canonical source URL used to register the project is invalid.
     ///
-    /// The 1.0 version of the whitepaper does not explain it means by a "valid"
-    /// URL, but it can be assumed tentatively that
+    /// The 1.0 version of the whitepaper establishes only one condition for
+    /// the validity of the URL - the source code retrieved from it must
+    /// always hash to the `hash` field of the `Project` structure -
+    /// but this definition of validity can be tentatively extended to include:
+    ///
     /// 1. it is a proper URL as defined in [RFC 3986](https:///tools.ietf.org/html/rfc3986#section-1.1.3), and
     /// 2. it hosts the repository's page in a distributed version control
-    ///   system's website e.g. GitLab, BitBucket, SourceForge, GitHub, and
+    ///    system's website e.g. GitLab, BitBucket, SourceForge, GitHub, and
     /// 3. it can be accessed without restrictions [*]
-    /// [*] This part can be harder to define - if the URL returns `404`s
-    /// *after* it has been inducted into the ledger, but not before, is it
-    /// still valid?
+    /// [*] This part can be harder to define - if the URL returns permanently
+    /// returns `404`s *after* it has been inducted into the ledger, but not
+    /// before, is it still valid?
     InvalidURLError(),
 }
 
@@ -69,7 +79,23 @@ pub enum KeysetError {
 pub enum UnregisterProjectError {}
 
 /// Errors that may occur when checkpointing a project.
-pub enum CheckpointError {}
+///
+/// Question:
+/// * Does an invalid dependency update list in a checkpoint invalidate it
+/// entirely?
+pub enum CheckpointError {
+    /// A dependency update is invalid if it adds a dependency the project
+    /// already uses.
+    UsedDependencyAddedError(),
+
+    /// A dependency update is invalid if it removes a dependency the project
+    /// does not use.
+    UnusedDependencyRemovedError(),
+
+    /// As the whitepaper says, a checkpoint is invalid if the dependency
+    /// update list containts duplicate dependencies.
+    DuplicateDependenciesError()
+}
 
 /// Datatype representing a hash-linked-list. Used in the whitepaper to
 /// organize contributions when checkpointing.
@@ -77,7 +103,7 @@ pub enum CheckpointError {}
 /// The type it abstracts over - in the context of the whitepaper's Ledger
 /// section, contributions, here abbreviated as `C` - should be a tuple,
 /// struct or equivalent with at least two fields e.g. `prev` and `commit`
-/// such that for every two consecutive members of the list
+/// such that for every two consecutive members of the hash-linked-list
 /// `C {prev1 = hash1, commit1 = hash2 .. }, C {prev2 = hash3, commit2 = hash4 ..}`:
 /// * it is true that `hash2 == hash3`;
 /// * if the `hash1` is the first hash present in the list, it is either
@@ -87,8 +113,8 @@ pub enum CheckpointError {}
 ///
 /// In practice, it may not necessarily be a list, but conceptually the name
 /// is explanatory.
-pub struct HashList <T> {
-    contributions : [T]
+pub struct HashLinkedList <T> {
+    contributions : PhantomData<T>
 }
 
 /// Representation of a contribution's author.
@@ -147,7 +173,6 @@ pub trait LedgerTransactions {
         -> Result<(), KeysetError> { unimplemented!() }
 
     /// Unregistering a project in the Oscoin Ledger.
-    /// 
     fn unregister_project ( project_address : Address)
         -> Result<(), UnregisterProjectError> { unimplemented!() }
 
@@ -155,7 +180,7 @@ pub trait LedgerTransactions {
     fn checkpoint ( project_address : Address
                   , new_project_hash : Hash
                   , project_url : URL
-                  , contribution_list : HashList<Contribution>
-                  , dependency_updates : [DependencyUpdate]
+                  , contribution_list : HashLinkedList<Contribution>
+                  , dependency_updates : Vec<DependencyUpdate>
                   ) -> Result<(), CheckpointError> { unimplemented!() }
 }
